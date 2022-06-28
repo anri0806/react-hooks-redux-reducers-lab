49bed995edc6bf927c044ca4aab9dcfe112b5a40
Anri <anri0806@hotmail.co.jp>
2022-06-27 22:59:00 -0400

    commit tree 12d9bb39f2d57e1347df893ce17bb0fde0b794d1

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,18 @@
 export function manageFriends(state, action) {
   // your code here
+
+  //initialState = { friends: []}
+
+  switch (action.type) {
+    case "friends/add":
+      return { friends: [...state.friends, action.payload] };
+    case "friends/remove":
+      return {
+        friends: state.friends.filter((friend) => friend.id !== action.payload),
+      };
+    default:
+      return state;
+  }
 }
+
+//{"friends": [{"hometown": "NYC", "id": 100, "name": "Avi"}]}
